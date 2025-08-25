b06943b065a9af8b6237a1ced8e22ff2fad03cf2
Shalin <shalinmodi60@gmail.com>
2025-08-25 12:54:39 +0530

    4th commit

--- controllers/BrandController.py
+++ controllers/BrandController.py
@@ -5,3 +5,7 @@ from config.database import brand_collection
 async def add_brand(brand: Brand):
 	result = brand_collection.insert_one(brand.dict())
 	return {**brand.dict}
+
+async def get_brands():
+    brands = await brand_collection.find().to_list(1000)
+    return [Brand(**brand) for brand in brands]
--- routes/BrandRoute.py
+++ routes/BrandRoute.py
@@ -2,13 +2,14 @@
 from fastapi import FastAPI,APIRouter
 app=FastAPI()
 from controllers.BrandController import add_brand
+from controllers.BrandController import get_brands
 from models.BrandModel import Brand
 router=APIRouter()
-
 @router.post("/add-brand")
 async def addBrand(brand:Brand):
 	return await add_brand(brand) #route logic
 
 
-
-	
+@router.get("/get-brands")
+async def getBrands():
+	return await get_brands()
