7d772a71fdcabaad2b2907e5203c5125ee9639fe
Shalin <shalinmodi60@gmail.com>
2025-08-27 21:15:26 +0530

    11th commit

--- controllers/TestController.py
+++ controllers/TestController.py
@@ -8,6 +8,13 @@ async def add_test_data(test: Test):
 async def delete_test_data(test_id: str):
     result=await test_collection.delete_one({"_id":ObjectId(test_id)})
     return {"message":"Test data deleted successfully"}
+
 async def get_all_tests():
     tests = await test_collection.find().to_list(1000)
     return [Test(**test) for test in tests]
+
+async def get_test_by_id(test_id: str):
+    result = await test_collection.find_one({"_id": ObjectId(test_id)})
+    if result:  # Remove MongoDB _id field
+        return Test(**result)
+    return None
